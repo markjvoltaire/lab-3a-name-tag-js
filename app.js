291aeaa794b400187794b50327f39694eb77ff79
Mark Voltaire <markjvoltaire@gmail.com>
2021-09-29 16:21:47 -0400

    Lab Completed

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // import functions and grab DOM elements
-
+const nameInput = document.getElementById('name');
+const firstName = document.getElementById('firstName');
+const button = document.getElementById('generate');
 // initialize global state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+button.addEventListener('click', ()=>{
+    let newName = nameInput.value;
+    firstName.textContent = newName;
+});
